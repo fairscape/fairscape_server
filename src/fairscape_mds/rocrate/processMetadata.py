f3c4c29fa03f0a4842d0a9b49e67770b64b2ae75
Max Levinson <max.adam.levinson@gmail.com>
2025-01-31 17:31:27 -0500

    Adding Comments to rocrate/proccessMetadata.py

--- src/fairscape_mds/rocrate/processMetadata.py
+++ src/fairscape_mds/rocrate/processMetadata.py
@@ -1,13 +1,27 @@
-from typing import List, Dict
+import json
+from typing import (
+	List,
+	Dict,
+	Callable
+)
+from fairscape_mds.models.rocrate import ROCrate
 
 class ROCrateFilterException(Exception):
-	def __init__(message, results):
+	def __init__(self, message, results):
 		self.results = results
 		self.message = message
 		super().__init__(self.message)
 
 
-def filterOneMetadataGraph(metadataGraph: List[Dict], filterFunc: Callable):
+def filterOneMetadataGraph(metadataGraph: List[Dict], filterFunc: Callable) -> Dict | None:
+	""" Given a metadataGraph filter the elements using a passed callable and return one element 
+
+	:param metadataGraph: input metadata graph
+	:type metadataGraph: List[Dict]
+	:param filterFunc: filter function to preform on the graph
+	:type filterFunc: Callable
+	:rtype: Dict | None
+	"""
 	metadataSearch = list(filter(filterFunc, metadataGraph))
 
 	if len(metadataSearch) < 1:
@@ -20,31 +34,55 @@ def filterOneMetadataGraph(metadataGraph: List[Dict], filterFunc: Callable):
 		return metadataElem
 
 
-def findRootElem(crateMetadata: Dict)->Dict:
+def findRootElem(crateMetadata: Dict)->dict:
+	""" Filter ROCrate metadata to find the root element representing the toplevel ROCrate 
+
+	:param crateMetadata: input metadata
+	:type crateMetadata: dict
+	:return: Root ROCrate Element
+	:rtype: Dict
+	"""
 	crateMetadataFilter = lambda x: x.get('@id') == 'ro-crate-metadata.json'
-	crateGUIDFilter = lambda elem: elem.get('@id') == rootMetadataGUID
 
 	# find the ro-crate-metadata.json elem
-	roCrateMetadataElem = filterOneMetadataGraph(crateMetadataNew['@graph'], crateMetadataFilter)
+	roCrateMetadataElem = filterOneMetadataGraph(crateMetadata['@graph'], crateMetadataFilter)
 	rootMetadataGUID = roCrateMetadataElem.get('about', {}).get('@id')
+	crateGUIDFilter = lambda elem: elem.get('@id') == rootMetadataGUID
 
-	rootMetadataElem = filterOneMetadataGraph(crateMetadataNew['@graph'], crateGUIDFilter)
-
+	rootMetadataElem = filterOneMetadataGraph(crateMetadata['@graph'], crateGUIDFilter)
 	return rootMetadataElem
 
-def getIndexGraphFilter(queryFunc: callable, graph: List):
-	indexList = [ i for i, elem in  enumerate(graph) if queryFunc(elem)]
+
+def getIndexGraphFilter(metadataGraph: List[Dict], queryFunc: Callable)-> int:
+	""" Return the Index of an element matching the query filter
+
+	:param metadataGraph: input metadata graph
+	:type metadataGraph: List[Dict]
+	:param queryFunc: filter function to 
+	:type queryFunc: Callable
+	:rtype: int
+	"""
+	indexList = [ i for i, elem in  enumerate(metadataGraph) if queryFunc(elem)]
 	if len(indexList) != 1:
 		raise Exception
 	else:
 		return indexList[0]
 
 
-def pruneROCrate(metadataGraph: List, filterFunc: callable):
-	elemIndex = getIndexGraphFilter(filterFunc, metadataGraph)
+def pruneROCrate(metadataGraph: List, filterFunc: Callable) -> None:
+	""" Filter and remove an element from the metadata graph as specified by the filter function 
+
+	:param metadataGraph: input metadata graph
+	:type metadataGraph: List[Dict]
+	:param queryFunc: filter function to 
+	:type queryFunc: Callable
+	:rtype: Int
+	"""
+	elemIndex = getIndexGraphFilter(metadataGraph, filterFunc)
 	metadataGraph.pop(elemIndex)
 
-def formatROCrateToModel(inputCrate: Dict)-> rocrate.ROCrate:
+
+def formatROCrateToModel(inputCrate: Dict)-> ROCrate:
 	""" Converts ROCrate v1.1 metadata into a fairscape ROCrate pydantic model
 
 	:param inputCrate: input metadata
@@ -63,10 +101,10 @@ def formatROCrateToModel(inputCrate: Dict)-> rocrate.ROCrate:
 	pruneROCrate(formattedCrateMetadataGraph, lambda x: x.get("@type") == 'Organization')
 
 	# set the metadata graph
-	rootMetadataElem['@graph'] = formattedCrateMetadataGraph
+	rootElem['@graph'] = formattedCrateMetadataGraph
 
 	# overwrite the metadata graph
-	rootMetadataElem['@type'] = 'https://w3id.org/EVI#ROCrate'
+	rootElem['@type'] = 'https://w3id.org/EVI#ROCrate'
 
 	# validate that root metadata elem works for rocrate
-	return rocrate.ROCrate.model_validate(rootMetadataElem)
+	return ROCrate.model_validate(rootElem)
